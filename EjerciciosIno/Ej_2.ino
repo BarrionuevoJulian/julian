856c0ca91d32605d99a32810883dc08ab78d6b69
BarriouevoJulian <barriojab@gmail.com>
2022-10-23 19:28:32 -0300

    Ej_2

---
+++ EjerciciosIno/Ej_2.ino
@@ -0,0 +1,36 @@
+// Maquina de estado 2
+
+#include <Arduino.h>
+#define LED LED_BUILTIN
+#define PULSADOR 3 // pin 3
+
+enum ESTADO
+{
+    inicial,
+    prendido,
+    apagado
+};
+
+/**
+ * @brief Maquina de estado para cambiar estado de prendido a apagado
+ * 
+ * @param led Pin del led
+ * @param repite Tiempo pren/apagado
+ */
+void mef(char led, int pulsador);
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    mef(LED, PULSADOR);
+    delay(1);
+}
+
+void mef(char led, int pulsador)
+{
+    static char ESTADO = inicial;
+}
