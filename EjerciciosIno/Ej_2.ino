5e3eeb3553d7ebe2a4bd34e92716598a0a897d47
BarriouevoJulian <barriojab@gmail.com>
2022-10-23 19:48:18 -0300

    Ej_2 en proceso

--- EjerciciosIno/Ej_2.ino
+++ EjerciciosIno/Ej_2.ino
@@ -40,5 +40,37 @@ void mef(char led, int pulsador)
         ESTADO = prendido;
         digitalWrite(led, HIGH);
     }
-    
+
 }
+
+/*
+
+if (millis >= led_lm)
+        {
+            gpio_toggle(GPIOB, GPIO11);
+            led_lm = millis + 500;
+        }
+        if (millis >= puls_lm)  
+        {
+            //  acumulo la última lectura y descarto la más vieja:
+            puls_est = puls_est << 1;
+            // leo el estado del pulsador:
+            if (gpio_get(GPIOB, GPIO9) != GPIO9)
+                puls_est = puls_est | 1;
+            // verifico si hubo 8 lecturas iguales
+            if (puls_est == 0b00000000)
+                estado_actual = 0;
+            else if (puls_est == 0b11111111)
+                estado_actual = 1;
+            // detección del flanco:
+            if (estado_actual > estado_anterior)
+            {
+                gpio_toggle(GPIOB, GPIO12);
+            }
+            // el proximo anterior es el actual:
+            estado_anterior = estado_actual;
+            // ejecuto denuevo el testeo en 7ms
+            puls_lm = millis + 7;
+        }
+
+*/
