b2517a471caa705218ae7bea58ad756a40594571
BarriouevoJulian <barriojab@gmail.com>
2022-10-30 02:27:14 -0300

    Pinche pulsador y tinkercad

--- EjerciciosIno/Ej_2.ino
+++ EjerciciosIno/Ej_2.ino
@@ -1,21 +1,15 @@
 // Maquina de estado 2
 
-#include <Arduino.h>
+// #include <Arduino.h>
 #define LED LED_BUILTIN
-#define PULSADOR 3 // pin 3
+#define PULSADOR 8 // pin 8
 
 enum EST
 {
     inicial,
     prendido,
-    apagado
-};
-
-enum LEC
-{
-    conteo,
-    muestreo,
-    retorno
+    apagado,
+	pulso
 };
 
 /**
@@ -25,89 +19,123 @@ enum LEC
  * @param repite Tiempo pren/apagado
  */
 void mef(char led, int pulsador);
-bool pulsoToggle(char pulsador);
+bool AntiRebotePulso(char pull);
 
-void setup(){}
+void setup(){
+	Serial.begin(9600);
+}
 
 void loop()
 {
-	bool pulsador = pulsoToggle (PULSADOR);
+	bool pulsador = AntiRebotePulso (PULSADOR);
+	Serial.println(pulsador);
     mef(LED, pulsador);
     delay(1);
 }
 
-void mef(char led, bool cambio)
+void mef(char led, bool pulsar)
 {
     static char EST = inicial;
+	static bool bandera = false;
+	static bool onoff;
 //----------------------------------------------------- inicial ---
     if (EST == inicial)
     {
         pinMode(led, OUTPUT);
-        EST = apagado;
-        digitalWrite(led, HIGH);
+        EST = prendido;
+        onoff = true;
     }
 //----------------------------------------------------- prendido ---
     if (EST == prendido) {
 		digitalWrite(led,HIGH);
+		onoff = true;
+		EST = pulso;
 	}
 //----------------------------------------------------- apagado ---
     if (EST == apagado) {
-		 digitalWrite(led, LOW);
+		digitalWrite(led, LOW);
+        onoff = false;
+		EST = pulso;
 	}
-//-----------------------------------------------------------------
-	if (cambio) EST = prendido;
-	else EST = apagado; 
+//------------------------------------------------------- pulso ---
+	if (EST == pulso){
+		if (pulsar == true && bandera == false){
+			if (onoff) EST = apagado;
+			else EST = prendido;
+			bandera = true;
+		} else if (!pulsar) bandera = false;
+	}
 }
 
-#define CICLO 7 // cada cuanto se leera el boton
+enum LEC
+{
+	config,
+    conteo,
+    muestreo,
+    retorno_true,
+	retorno_false
+};
 
-uint8_t lectura = 0xFF; // igual a 0b11111111
-int ciclo = 0;
+#define CICLO 5 // cada cuanto se leera el boton
 
 /**
- * @brief Lee pulsador y togglea valor de su función cuando se presiona el mismo.
+ * @brief Lee pulsador y devuelve sin rebote.
  * muestro del mismo cada CICLOS ms.
  * 
- * @param pulsador Pin del pulsador
+ * @param pull Pin del pulsador
  * @return true 
  * @return false 
  */
-bool pulsoToggle(char pulsador)
+bool AntiRebotePulso(char pull)
 {
-	static char LEC = conteo;
+	static char LEC = config;
 
-	static uint8_t lectura = 0xFF;      // igual a 0b11111111
+	static uint8_t lectura = 0x00;  // igual a 0b00000000
 	static int ciclo = 0;
 	static bool valorDevuelto = false;
 	
 	static bool cambio = false, anterior_cam = false;
-	
+//----------------------------------------------------- config ----
+	if (LEC == config){
+		pinMode(pull, INPUT_PULLUP);
+		digitalWrite(pull, HIGH);
+		LEC = conteo;
+	}
+//----------------------------------------------------- conteo -----
 	if (LEC == conteo) ciclo++;
-	
 	if (LEC == conteo && ciclo > CICLO) LEC = muestreo;
-
+//----------------------------------------------------- muestreo ---
 	if (LEC == muestreo)
 	{
 		lectura = lectura << 1;  // Mover de lugar el bit
-		if (digitalRead(pulsador) != 0){
-			lectura = lectura + 1;
+		if (digitalRead(pull) == 0){
+			lectura = lectura | 1;
 		}
 		if(lectura == 0){
 			cambio = false;
+			LEC = retorno_false;
 		}
-		else if(lectura == 0xFF){  // otra forma de poner 0b11111111
+		if(lectura == 0xFF){  // otra forma de poner 0b11111111
 			cambio = true;
+			LEC = retorno_true;
+		}/*
+		if(cambio > anterior_cam){
+			LEC = retorno_true;
 		}
 		if(cambio < anterior_cam){
-			// digitalWrite(led, !digitalRead(led));
-			LEC = retorno;
-		}
+			LEC = retorno_false;
+		}*/
 		anterior_cam = cambio;
 		ciclo = 0;
 	}
-
-	if (LEC == retorno){
+//----------------------------------------------------- retorno_true -
+	if (LEC == retorno_true){
 		LEC = conteo;
-		return valorDevuelto = !valorDevuelto;
+		return valorDevuelto = true;
+	}
+//---------------------------------------------------- retorno_false -
+	if (LEC == retorno_false){
+		LEC = conteo;
+		return valorDevuelto = false;
 	}
 }
