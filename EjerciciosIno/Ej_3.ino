a36c480a4cf40c61b7d9814b8f3afb9bca976ee4
BarrionuevoJulian <89553631+BarrionuevoJulian@users.noreply.github.com>
2022-11-09 14:14:52 -0300

    Arreglos necesarios

--- EjerciciosIno/Ej_3.ino
+++ EjerciciosIno/Ej_3.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#define LED_1 3
+#define LED_1 2
 #define LED_2 4
 
 enum ESTADO
@@ -16,7 +16,7 @@ void setup(){}
 
 void loop()
 {
-    brilloLeds_1(LED_1, 1);
+    brilloLeds_1(LED_1, 0);
     brilloLeds_2(LED_2, 5);
     delay(1);
 }
@@ -31,7 +31,6 @@ void brilloLeds_1( char led , int brillo )
 {
     static ESTADO est_1 = inicial;
     static int contador = 0;
-    static bool bandera = false;
 //----------------------------------------------------- inicial ---
     if (est_1 == inicial){
         pinMode(led, OUTPUT);
@@ -41,24 +40,22 @@ void brilloLeds_1( char led , int brillo )
 //----------------------------------------------------- prendido --
     if (est_1 == prendido){
         // si la bandera es verdadera y contador mayor igual a parada
-        if(contador >= brillo && bandera){
+        if(contador >= brillo){
             digitalWrite(led, LOW);
 			est_1 = apagado;
-            bandera = false;
 		}
 	}
 //----------------------------------------------------- apagado ---
 	if (est_1 == apagado){
 		// si la bandera es false y contador menor a parada
-		if (contador < brillo && !bandera){
+		if (contador < brillo){
 			digitalWrite(led, HIGH);
 			est_1 = prendido;
-			bandera = true;
 		}
 	}
     // contador vuelve a resetear el periodo
-    if(contador > 10) contador = 0;
     contador++;
+    if(contador > 9) contador = 0;
 }
 
 /**
@@ -69,34 +66,32 @@ void brilloLeds_1( char led , int brillo )
  */
 void brilloLeds_2( char led , int brillo )
 {
-    static ESTADO est_2 = inicial;
-    static int contador = 0;
-    static bool bandera = false;
-//----------------------------------------------------- inicial ---
-    if (est_2 == inicial){
-        pinMode(led, OUTPUT);
+	static ESTADO est_2 = inicial;
+	static int contador = 0;
+	//----------------------------------------------------- inicial ---
+	if (est_2 == inicial){
+		pinMode(led, OUTPUT);
 		digitalWrite(led, prendido);
-        est_2 = prendido;
-    }
-//----------------------------------------------------- prendido --
-    if (est_2 == prendido){
-        // si la bandera es verdadera y contador mayor igual a parada
-        if(contador >= brillo && bandera){
-            digitalWrite(led, LOW);
+		est_2 = prendido;
+	}
+	//----------------------------------------------------- prendido --
+	if (est_2 == prendido){
+		// si la bandera es verdadera y contador mayor igual a parada
+		if(contador >= brillo){
+			digitalWrite(led, LOW);
 			est_2 = apagado;
-            bandera = false;
 		}
 	}
-//----------------------------------------------------- apagado ---
+	//----------------------------------------------------- apagado ---
 	if (est_2 == apagado){
 		// si la bandera es false y contador menor a parada
-		if (contador < brillo && !bandera){
+		if (contador < brillo){
 			digitalWrite(led, HIGH);
 			est_2 = prendido;
-			bandera = true;
 		}
 	}
-    // contador vuelve a resetear el periodo
-    if(contador > 10) contador = 0;
-    contador++;
+	// contador vuelve a resetear el periodo
+	contador++;
+	if(contador > 9) contador = 0;
 }
+
